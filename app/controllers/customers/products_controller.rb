3e5a99fa59972807da242c91dda6fe9ea7468977
Hara Tomoka <pixieyunomi@gmail.com>
2020-10-05 14:45:23 +0900

    add category-box

--- app/controllers/customers/products_controller.rb
+++ app/controllers/customers/products_controller.rb
@@ -1,9 +1,20 @@
 class Customers::ProductsController < ApplicationController
 
 	def top
+		@categories = Category.all
+		@products = Product.all
 	end
 
 	def index
+		@products = Product.all
+		@categories = Category.all
+		if params[:category_id]
+			@category = Category.find(params[:category_id])
+			@products = @products.where(category_id: params[:category_id])
+		end
+	end
+
+	def show
 	end
 
 	def about
--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '6b0767b25165b7fd8ece0be693a0da26a6c13eead2dc4f9d8106369cf69b31248d4ec4ed2afe3daf479e826a3c79fc7d78e08f4332e061fd0ed304ebf83835a0'
