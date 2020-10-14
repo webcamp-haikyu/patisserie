dd693ec95a82274ee3a10bebec5e8f4506d73e9f
vagrant <vagrant@localhost.localdomain>
2020-10-14 07:41:54 +0000

    ログアウト修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,10 +4,18 @@ class ApplicationController < ActionController::Base
 	def after_sign_in_path_for(resource)
 		case resource
 		when Admin
-			admins_admins_top
+             admins_root_path
 		when Customer
-			products_path
+             root_path
 		end
+	end
+
+	def after_sign_out_path_for(resource_or_scope)
+	    if resource_or_scope == :admin
+	       new_admin_session_path
+	    else
+	       root_path
+	    end
 	end
 
 	protected
